5e210e91ee292874f0610eb44b5164f29cb07092
Luis Robles <luisgeremiasrobles@gmail.com>
2025-06-21 16:32:51 -0300

    Se agregaron los ejercicios 1 y 2

--- js/Tp3-Ejercicio1.js
+++ js/Tp3-Ejercicio1.js
@@ -1,1 +1,21 @@
-alert("Hola Mundo");
+//Crear un array llamado meses y que almacene el nombre de los doce meses del
+//año. Mostrar por pantalla en forma de lista los doce nombres del arreglo.
+const Meses = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+document.writeln(`<h2 class="ms-4">Lista de meses</h2>`);
+document.writeln(`<ul class="ms-3">`);
+for (let i = 0; i < Meses.length; i++) {
+  document.writeln(`<li><strong>${Meses[i]}</strong></li>`);
+}
+document.writeln(`</ul>`);
