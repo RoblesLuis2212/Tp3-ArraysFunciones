91d949ce53752b255aa8d4a912533c7e3864f2d5
Luis Robles <luisgeremiasrobles@gmail.com>
2025-06-21 19:49:08 -0300

    Se intento hacer el Ejercicio 3 de los dados

--- js/Tp3-Ejercicio3.js
+++ js/Tp3-Ejercicio3.js
@@ -2,3 +2,8 @@
 //función Math.random para obtener números aleatorios entre 1 y 6 para cada uno
 //de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados y
 //anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación.
+
+const dado1 = Math.floor(Math.random() * 6) + 1;
+const dado2 = Math.floor(Math.random() * 6) + 1;
+
+const suma = dado1 + dado2;
